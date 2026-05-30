a3a37a9f7edcee6b97a57e02072675838b3a02f1
Sutasinee Meeon <chfyf2585@gmail.com>
2026-05-30 12:15:27 +0700

    Update postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,9 @@
+// postcss.config.js
 export default {
   plugins: {
+    // ประมวลผล Tailwind CSS
     tailwindcss: {},
+    // เติม CSS Prefixes อัตโนมัติเพื่อให้รองรับเบราว์เซอร์ที่หลากหลาย
     autoprefixer: {},
   },
-}
+};
